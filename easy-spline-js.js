eaf8b7c0a2b6dcd741a110333a019f776ea344ae
Arkadii Neskorod <a_neskorod@viyar.com.ua>
2018-08-16 16:36:26 +0300

    create easy-layers-js for control layers. easible spline usage.

--- easy-spline-js.js
+++ easy-spline-js.js
@@ -1,18 +1,11 @@
 
-CanvasRenderingContext2D.prototype.cub_spline = ({
-	canvas: null,
-	splines: [],
-
-	down: false,
-	selected: false,
-	selected_line: false,
-
-	strong_matrix: [
+(function() {
+	this.strong_matrix = [
 		[1, 0, 0],
 		[-3, 4, -1],
 		[2, -4, 2]
-	],
-	mul_to_strong: function(m) {
+	];
+	this.mul_to_strong = function(m) {
 		var res = [0,0,0];
 		for(var i in res) {
 			for(var j in this.strong_matrix) {
@@ -20,8 +13,8 @@ CanvasRenderingContext2D.prototype.cub_spline = ({
 			}
 		}
 		return res;
-	},
-	build: function(points, start, end) {
+	};
+	this.build = function(points, start, end) {
 		var x_m = [];
 		var y_m = [];
 
@@ -67,222 +60,119 @@ CanvasRenderingContext2D.prototype.cub_spline = ({
 					this.i += step;
 					return calc(this.start);
 				}
-				for(;this.i <= this.end + step;) {
+				for(;this.i <= this.end;) {
 					this.i += step;
 					return calc(this.i);
 				}
 				return false;
 			}
-		}
-		// function(ctx) {
-		// 	ctx.moveTo(pos[0], pos[1]);		
-		// 	i += step;
-		// 	for(;i <= end + step; i += step) {
-		// 		pos = calc(i);
-		// 		ctx.lineTo(pos[0], pos[1]);
-		// 	}
-		// }
-	},
-	cross: function(keys, start, end, pos) {
-		var x_m = [];
-		var y_m = [];
+		};
+	};
 
-		for(var i in keys) {
-			x_m.push(keys[i][0]);
-			y_m.push(keys[i][1]);
-		}
+	this.crossPoints = function(point1, point2) {
+		return point1[0] > point2[0] - 5 && point1[0] < point2[0] + 5 && 
+				point1[1] > point2[1] - 5 && point1[1] < point2[1] + 5
+	};
 
-		var x_p = mul_to_strong(x_m);
-		var y_p = mul_to_strong(y_m);
+	var self = this;
 
-		var calc = i => {
-			var x = x_p[0] + x_p[1]*i + x_p[2]*i*i;
-			var y = y_p[0] + y_p[1]*i + y_p[2]*i*i;
-			return [x, y];
-		}
-
-		var step = 0.01;
-		i = start + step;
-		for(;i <= end + step; i += step) {
-			var posi = calc(i);
-			if(pos[0] > posi[0] - 5 && pos[0] < posi[0] + 5 && pos[1] > posi[1] - 5 && pos[1] < posi[1] + 5) {
-				return true;
-			}
-		}
-		return false;
-	},
-
-	mousedown: function(e) {
-		var pos = [e.layerX, e.layerY];
-		if(e.button === 0 && this.selected_line === false) {
-			this.down = true;
-			if(!this.selected) {
-				//points.push([e.layerX, e.layerY]);
-				//draw();
-			}
-
-			this.splines[0].add(pos[0], pos[1])
-		} else if(e.button == 2 && this.selected_line !== false) {
-			this.down = false;
-			//points.splice(selected_line+1, -1, pos);
-			//draw();
-		}
-	},
-	mousemove: function(e) {
-		var pos = [e.layerX, e.layerY];
-		if(this.down && this.selected) {
-			canvas.style.cursor = 'grabbing';
-			this.selected[0] = pos[0];
-			this.selected[1] = pos[1];
-			// draw();
-		} else {
-			// for(var i in points) {
-			// 	var posi = points[i];
-			// 	if(pos[0] > posi[0] - 6 && pos[0] < posi[0] + 6 && pos[1] > posi[1] - 6 && pos[1] < posi[1] + 6) {
-			// 		selected = points[i];
-			// 		canvas.style.cursor = 'grab';
-			// 		if(selected_line) {
-			// 			selected_line = false;
-			// 			draw();
-			// 		}
-			// 		return;
-			// 	}
-			// 	canvas.style.cursor = 'default';
-			// 	selected = false;
-			// }
-			// if(points.length > 2) {
-			// 	for(var i = 0; i < points.length - 3; i++) {
-			// 		if(crossSpline([points[i], points[i+1], points[i+2]], 0, 0.5, pos)) {
-			// 			selected_line = i;
-			// 			draw();
-			// 			return;
-			// 		}
-			// 	}
-			// 	if(crossSpline([points[i], points[i+1], points[i+2]], 0, 0.5, pos)) {
-			// 		selected_line = i;
-			// 		draw();
-			// 		return;
-			// 	}
-			// 	if(crossSpline([points[i], points[i+1], points[i+2]], 0.5, 1, pos)) {
-			// 		selected_line = i+1;
-			// 		draw();
-			// 		return;
-			// 	}
-			// 	if(selected_line !== false) {
-			// 		selected_line = false;
-			// 		draw();
-			// 	}
-			// }
-		}
-	},
-	mouseup: function(e) {
-		this.down = false;
-		this.selected = false;
-	},
-	initEvents: function(canvas) {
-		canvas.oncontextmenu = e => { e.preventDefault(); };
-		canvas.addEventListener('mousedown', this.mousedown.bind(this), false);
-		canvas.addEventListener('mousemove', this.mousemove.bind(this), false);
-		canvas.addEventListener('mouseup',   this.mouseup.bind(this),   false);
-	},
-
-	Spline: function(controller) {
-		this.points = [];
-		this.ctx = null;
-		this.draw = function() {
-			this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
-			this.ctx.lineWidth = 3;
-			this.ctx.strokeStyle = "blue";
-			var clear = 0;
-			for(var i = 0; i < this.points.length - 3; i++) {
-				this.ctx.beginPath();
-				var sp = controller.build([
-					this.points[i], 
-					this.points[i+1], 
-					this.points[i+2]
-				], 0, 0.5);
-				var pos = sp.next();
-				this.ctx.moveTo(pos[0], pos[1]);
-				while(pos = sp.next()) {
-					this.ctx.lineTo(pos[0], pos[1]);
-				}
-				this.ctx.stroke();
-			}
-			for(var j = this.points.length; j < 3; j++) {
-				if(j > 0) {
-					this.points.push(this.points[j-1])
-				}else{
-					this.points.push([0,0])
-				}
-				clear++;
-			}
-			this.ctx.beginPath();
-			var sp = controller.build([
-				this.points[i], 
-				this.points[i+1], 
-				this.points[i+2]
+	CanvasRenderingContext2D.prototype.spline = function(points) {
+		var sp;
+		var pos;
+		var i = 0;
+		for(; i < points.length - 3; i++) {
+			sp = self.build([
+				points[i],
+				points[i+1],
+				points[i+2]
 			], 0, 0.5);
-			var pos = sp.next();
-			this.ctx.moveTo(pos[0], pos[1]);
+			pos = sp.next();
+			this.moveTo(pos[0], pos[1]);
 			while(pos = sp.next()) {
-				this.ctx.lineTo(pos[0], pos[1]);
+				this.lineTo(pos[0], pos[1]);
 			}
-			this.ctx.stroke();
-			this.ctx.beginPath();
-			sp.start = 0.5;
-			sp.end = 1;
-			var pos = sp.next();
-			this.ctx.moveTo(pos[0], pos[1]);
-			while(pos = sp.next()) {
-				this.ctx.lineTo(pos[0], pos[1]);
-			}
-			this.ctx.stroke();
-			for(var i = 0; i < clear; i++) {
-				this.points.splice(this.points.length - clear, 1);
-			}
-
-			for(var k in this.points) {
-				this.ctx.beginPath();
-				this.ctx.arc(this.points[k][0], this.points[k][1], 5, 0, Math.PI * 2);
-				this.ctx.fill();
-			}
-		};
-		this.add = function(x, y) {
-			this.points.push([x, y]);
-			this.draw();
-		};
-
-		// constructor
-		(function() {
-			var canvas = document.createElement('canvas');
-			controller.initEvents(canvas);
-			canvas.width = controller.canvas.width;
-			canvas.height = controller.canvas.height;
-			canvas.style.position = 'absolute';
-			canvas.style.left = controller.canvas.clientLeft + 'px';
-			canvas.style.top = controller.canvas.clientTop + 'px';
-			controller.canvas.parentNode.style.position = 'relative';
-			if(controller.canvas.nextSibling) {
-				controller.canvas.parentNode.insertBefore(canvas, controller.canvas.nextSibling);
-			} else {
-				controller.canvas.parentNode.appendChild(canvas);
-			}
-			this.ctx = canvas.getContext('2d');
-			this.draw();
-		}).bind(this)()
-	},
-
-	create() {
-		if(!this.canvas) {
-			console.error('please init with context 2d');
-			return;
 		}
+		// if lentgth < 3 init clear (add fake)
+		var clear = 0;
+		for(var j = points.length; j < 3; j++) {
+			if(j > 0) {
+				points.push(points[j-1]);
+			} else {
+				points.push([0, 0]);
+			}
+			clear++;
+		}
+		sp = self.build([
+			points[i],
+			points[i+1],
+			points[i+2]
+		], 0, 0.5);
+		while(pos = sp.next()) {
+			this.lineTo(pos[0], pos[1]);
+		}
+		sp.start = 0.5;
+		sp.end   = 1;
+		while(pos = sp.next()) {
+			this.lineTo(pos[0], pos[1]);
+		}
+		// clear (rm fake)
+		while(clear > 0) {
+			points.splice(points.length - 1, 1);
+			clear--;
+		}
+	};
 
-		return this.splines[this.splines.push(new this.Spline(this))];
-	},
-
-	init: function(ctx) {
-		this.canvas = ctx.canvas;
-		// this.initEvents();
-	}
-})
+	CanvasRenderingContext2D.prototype.crossSpline = function(points, point) {
+		var sp;
+		var pos;
+		var i = 0;
+		for(; i < points.length - 3; i++) {
+			sp = self.build([
+				points[i],
+				points[i+1],
+				points[i+2]
+			], 0, 0.5);
+			while(pos = sp.next()) {
+				if(self.crossPoints(pos, point)) {
+					return i;
+				}
+			}
+		}
+		var clear = 0;
+		var nullClear = function() {
+			while(clear > 0) {
+				points.splice(points.length - 1, 1);
+				clear--;
+			}
+		};
+		for(var j = points.length; j < 3; j++) {
+			if(j > 0) {
+				points.push(points[j-1]);
+			} else {
+				points.push([0, 0]);
+			}
+			clear++;
+		}
+		sp = self.build([
+			points[i],
+			points[i+1],
+			points[i+2]
+		], 0, 0.5);
+		while(pos = sp.next()) {
+			if(self.crossPoints(pos, point)) {
+				nullClear();
+				return i;
+			}
+		}
+		sp.start = 0.5;
+		sp.end = 1;
+		while(pos = sp.next()) {
+			if(self.crossPoints(pos, point)) {
+				nullClear();
+				return i+1;
+			}
+		}
+		nullClear();
+		return false
+	};
+})()
--- main.js
+++ main.js
@@ -8,6 +8,25 @@ document.body.style.background = '#000000';
 canvas.style.background = 'gray';
 
 var ctx = canvas.getContext('2d');
-ctx.cub_spline.init(ctx)
+var layer1 = ctx.layer()
 
-var spline = ctx.cub_spline.create()
+ctx.handle('mousedown', function(e) {
+    var pos = [e.layerX, e.layerY];
+    var i = layer1.crossSpline(points, pos);
+    if(i === false) {
+        points.push(pos);
+    } else {
+        points.splice(i+1, -1, pos)
+    }
+    draw();
+})
+
+var points = [];
+
+function draw() {
+    layer1.clearRect(0, 0, canvas.width, canvas.height);
+    layer1.beginPath();
+    layer1.spline(points);
+    layer1.stroke();
+}
+
